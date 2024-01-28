0788aa28bf4dfe2fa049f8c9ee1a5bd4739b7d77
swtt-pch <i.greygomes3@gmail.com>
2024-01-28 12:14:17 -0300

    chore: understanding file colocation

---
+++ routing-demo/src/app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+function BarChart() {
+  return (
+    <>
+      <h1>Bar Chart</h1>
+    </>
+  )
+}
+
+function Dashboard() {
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  )
+}
+
+export default Dashboard
---
+++ routing-demo/src/components/line-chart.tsx
@@ -0,0 +1,10 @@
+function LineChart() {
+  return (
+    <>
+      <h2>Line Chart</h2>
+      <p>Line chart will be here</p>
+    </>
+  )
+}
+
+export default LineChart
