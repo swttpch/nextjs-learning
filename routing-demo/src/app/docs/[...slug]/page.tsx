db1d935cc15a430946b4c483865e25a88ecd24b6
swtt-pch <i.greygomes3@gmail.com>
2024-01-28 11:57:07 -0300

    chore: understanding catch all segments

---
+++ routing-demo/src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,25 @@
+function Page({ params }: { params: { slug: string[] } }) {
+  if (params.slug.length === 2) {
+    return (
+      <>
+        <h1>
+          Details for features {params.slug[0]} and concept {params.slug[1]}
+        </h1>
+      </>
+    )
+  } else if (params.slug.length === 1) {
+    return (
+      <>
+        <h1>Details for feature {params.slug[0]}</h1>
+      </>
+    )
+  }
+
+  return (
+    <>
+      <h1>Docs home page</h1>
+    </>
+  )
+}
+
+export default Page
