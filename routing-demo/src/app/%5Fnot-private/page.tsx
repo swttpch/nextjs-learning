749fa9588d97ee3fb80803c3e27825c393457b83
swtt-pch <i.greygomes3@gmail.com>
2024-01-28 12:18:56 -0300

    chore: understanding private folders

---
+++ routing-demo/src/app/%5Fnot-private/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <>
+      <h1>Not private page</h1>
+    </>
+  )
+}
+
+export default Page
---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <>
+      <h1>Private page</h1>
+    </>
+  )
+}
+
+export default Page
