d99ab56e6473ac7a225b52dd4132e71c38d8c995
swtt-pch <i.greygomes3@gmail.com>
2024-01-28 12:09:05 -0300

    chore: understanding custom not found and handling not found in pages

---
+++ routing-demo/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <div>
+      <h2> Page not fould</h2>
+      <p>Could not find requested resource</p>
+    </div>
+  )
+}
+
+export default NotFound
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,9 @@
+import { notFound } from 'next/navigation'
+
 function Page({ params }: { params: { productId: string; reviewId: string } }) {
+  if (parseInt(params.reviewId) > 1000) {
+    notFound()
+  }
   return (
     <>
       <h1>
